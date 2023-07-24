448707b3727990035491e5d4e1f1cbff31851d6c
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-24 16:41:27 -0300

    feat: "header of project and add pic of cogumelo"

--- app.py
+++ app.py
@@ -12,4 +12,14 @@ root.geometry('700x400+300+200')
 root.resizable(False,False)
 root.configure(bg='#326273') #cor de fundo
 
+#edit icon
+icon_image=PhotoImage(file="cogumelo.png")
+root.iconphoto(False,icon_image)
+
+#heading or header
+Label(root, text="Please, Complete full this form", font="arial 14", bg="#fff", fg="#000").place(x=20,y=20)
+
+#label
+
+
 root.mainloop()
