d720ce2b6472fee041849a471a2c4e64de98d4b2
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-24 16:33:00 -0300

    feat: "is running"

--- app.py
+++ app.py
@@ -1,4 +1,14 @@
 from tkinter import *
 from tkinter.ttk import Combobox
 from tkinter import messagebox
+import tkinter as tk
 import openpyxl ,xlrd
+from openpyxl import workbook
+import pathlib
+
+root=Tk()
+root.title("Data Entry")
+root.geometry('700x400+300+200')
+root.resizable(False,False)
+
+root.mainloop()
