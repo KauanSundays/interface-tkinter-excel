9f4625a5977879c6ce0dae93dfa153eb956f030c
Kauan Sundays <102335585+KauanSundays@users.noreply.github.com>
2023-07-25 17:52:50 -0300

    feat: "inputs in project"

--- app.py
+++ app.py
@@ -26,4 +26,22 @@ Label(root, text='Age', font=23, bg="#000", fg='#fff').place(x=50, y=200)
 Label(root, text='Gender', font=23, bg="#000", fg='#fff').place(x=370, y=200)
 Label(root, text='Address', font=23, bg="#000", fg='#fff').place(x=50, y=250)
 
+#entry
+
+nameValue = StringVar()
+contactValue = StringVar()
+AgeValue = StringVar()
+
+nameEntry = Entry(root, textvariable=nameValue, width=45, bd=2, font=20)
+contactEntry = Entry(root, textvariable=nameValue, width=45, bd=2, font=20)
+AgeEntry = Entry(root, textvariable=nameValue, width=15, bd=2, font=20)
+
+#gender
+gender_combobox =  Combobox(root,values=['Male', 'Female'], font='arial 14', state='r', width=14)
+gender_combobox.place(x=400, y=200)
+
+nameEntry.place(x=110,y=100)
+contactEntry.place(x=110,y=150)
+AgeEntry.place(x=110,y=200)
+
 root.mainloop()
